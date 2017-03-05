39842c91ff6d7c7a240e98a1ce3c49da906f79a0
emily wilson <emerlily.w@gmail.com>
2017-03-04 16:10:46 -0800

    added kill decision

--- app/main.py
+++ app/main.py
@@ -113,24 +113,26 @@ def move():
         
     else: #keep doing other stuff #TODO: Implement Snake behavioural AI
 
-    for snake in snakes:
-        for dir,coord in adjacent.items():
-            if snake['coords'] == coord
-                if len(my_snake['coord']) > len(snake['coord'])
-                    kill_flag = true
-                    taunt = 'kill flag'
-                    break
-                else
-                    kill_flag = false
-        break
-
-
-
+        for snake in snakes:
+            for dir,coord in adjacent.items():
+                if snake['coords'] == coord:
+                    if len(my_snake['coord']) > len(snake['coord']):
+                        kill_flag = True
+                        saveDir = dir
+                        taunt = 'kill flag'
+                        break
+                    else:
+                        kill_flag = False
+            break
 
 
         #pick and send move
         directions = {'up': 'up', 'down':'down', 'left':'left', 'right':'right'}
-        move = viable_move.keys()[0]
+        if kill_flag == True:
+            if saveDir in viable_move:
+             move = saveDir
+        else:
+            move = random.choice(viable_move.key())
     return {
         'move': move,
         'taunt': taunt
